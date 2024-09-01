00eac4e5ae3f2234c19140d51a96ac2672687fcc
Yash <yashgoel585@gmail.com>
2024-09-01 22:28:55 +0530

    again

---
+++ rotate_matrix.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+void rotate(vector<vector<int>>& matrix) {
+    int row = matrix.size(), col = matrix[0].size();
+        
+    for(int i = 0; i < row; i++) {
+        
+        for(int j = i; j < col; j++) { swap(matrix[i][j], matrix[j][i]); }
+    }
+
+    for(int i = 0; i < row; i++) { reverse(matrix[i].begin(), matrix[i].end()); }
+}
+
+int main() {
+    vector<vector<int>> matrix {{1,2,3},{4,5,6},{7,8,9}};
+    rotate(matrix);
+    return 0;
+}
+
