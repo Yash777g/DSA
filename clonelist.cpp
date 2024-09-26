a39dd2fe9ab6f02f5e8817bc637c3e13a85cd7a7
Yash <yashgoel585@gmail.com>
2024-09-26 14:14:58 +0530

    daily

--- clonelist.cpp
+++ clonelist.cpp
@@ -32,40 +32,64 @@ void print(Node* head) {
     }cout << endl;
 }
 
+// intertwen nodes
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (!head) return nullptr;
+        
+        Node* curr = head;
+        while (curr) {
+            Node* new_node = new Node(curr->val);
+            new_node->next = curr->next;
+            curr->next = new_node;
+            curr = new_node->next;
+        }
+        
+        curr = head;
+        while (curr) {
+            if (curr->random) {
+                curr->next->random = curr->random->next;
+            }
+            curr = curr->next->next;
+        }
+        
+        Node* old_head = head;
+        Node* new_head = head->next;
+        Node* curr_old = old_head;
+        Node* curr_new = new_head;
+        
+        while (curr_old) {
+            curr_old->next = curr_old->next->next;
+            curr_new->next = curr_new->next ? curr_new->next->next : nullptr;
+            curr_old = curr_old->next;
+            curr_new = curr_new->next;
+        }
+        
+        return new_head;       
+    }
+};
+
 // map approach
-Node* copyRandom(Node* head) {
-    Node* copyhead = head;
-    Node* copytail = copyhead;
-    Node* temp = head->next;
-
-    while(temp) {
-        Node* newNode = new Node(temp->val);
-
-        copytail->next = newNode;
-        copytail = newNode;
-        temp = temp->next;
+Node* copyRandomList(Node* head) {
+    if (!head) return nullptr;
+        
+    unordered_map<Node*, Node*> old_to_new;
+        
+    Node* curr = head;
+    while (curr) {
+        old_to_new[curr] = new Node(curr->val);
+        curr = curr->next;
     }
-
-    unordered_map<Node*,Node*> mapping;
-    temp = head;
-    Node* temp2 = copyhead;
-
-    while(temp && temp2) {
-        mapping[temp] = copyhead;
-        temp = temp->next;
-        temp2 = temp2->next;
+        
+    curr = head;
+    while (curr) {
+        old_to_new[curr]->next = old_to_new[curr->next];
+        old_to_new[curr]->random = old_to_new[curr->random];
+        curr = curr->next;
     }
-
-    temp = head;
-    temp2 = copyhead;
-
-    while(temp) {
-        temp2->random = mapping[temp->random];
-        temp = temp->next;
-        temp2 = temp2->next;
-    }
-
-    return copyhead;
+        
+    return old_to_new[head];
 }
 
 int main() {
@@ -78,7 +102,7 @@ int main() {
     insert(tail, 5);
     print(head);
 
-    head = copyRandom(head);
+    head = copyRandomList(head);
     print(head);
     return 0;
 }
