b3855b6e37651b93a5c6ad939a82f9897b86153b
Yash <yashgoel585@gmail.com>
2024-10-29 12:50:27 +0530

    daily

--- first_last.cpp
+++ first_last.cpp
@@ -21,38 +21,46 @@ using namespace std;
 
 // O(log(n)) time complexity
 int leftSide(vector<int> nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int ans = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (target == nums[mid]) {
+    int start = 0, end = nums.size() - 1;
+    int ans = -1;
+    
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if(target == nums[mid]) {
                 ans = mid;
                 end = mid - 1;
-            } else if (target < nums[mid]) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
-        return ans;
+        } 
+        
+        else if(target < nums[mid]) 
+            end = mid - 1;
+         
+        else 
+            start = mid + 1;     
     }
 
-    int rightSide(vector<int> nums, int target) {
-        int start = 0, end = nums.size() - 1;
-        int ans = -1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target) {
-                ans = mid;
-                start = mid + 1;
-            } else if (nums[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
+    return ans;
+}
+
+int rightSide(vector<int> nums, int target) {
+    int start = 0, end = nums.size() - 1;
+    int ans = -1;
+    
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            ans = mid;
+            start = mid + 1;
         }
-        return ans;
+        
+        else if(nums[mid] > target) 
+            end = mid - 1;
+         
+        else 
+            start = mid + 1;
+            
     }
+    return ans;
+}
 
     vector<int> searchRange(vector<int>& nums, int target) {
         int left = leftSide(nums, target);
