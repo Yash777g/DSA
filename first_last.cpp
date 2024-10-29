3976791b9a329cf4a464ef40dcc225888d5d888b
Yash <yashgoel585@gmail.com>
2024-10-29 12:50:56 +0530

    daily

--- first_last.cpp
+++ first_last.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 using namespace std;
 
-// O(n) time complexity
-// vector<int> searchRange(vector<int>& nums, int target) {
-//     vector<int> index = {-1,-1};
+//O(n) time complexity
+/*vector<int> searchRange(vector<int>& nums, int target) {
+    vector<int> index = {-1,-1};
 
-//     for(int i = 0; i < nums.size(); i++) {
-//         // little optimization
-//         if(nums[i] > target) { break; }
+    for(int i = 0; i < nums.size(); i++) {
+        // little optimization
+        if(nums[i] > target) { break; }
 
-//         if(nums[i] == target) {
-//             index[0] = i;
-//             index[1] = i + 1;
-//         }
-//     }
+        if(nums[i] == target) {
+            index[0] = i;
+            index[1] = i + 1;
+        }
+    }
 
-//     return index;
-// }
+    return index;
+}*/
 
 // O(log(n)) time complexity
 int leftSide(vector<int> nums, int target) {
@@ -51,22 +51,23 @@ int rightSide(vector<int> nums, int target) {
             ans = mid;
             start = mid + 1;
         }
-        
+
         else if(nums[mid] > target) 
             end = mid - 1;
          
         else 
-            start = mid + 1;
-            
+            start = mid + 1;     
     }
+    
     return ans;
 }
 
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int left = leftSide(nums, target);
-        int right = rightSide(nums, target);
-        return {left, right};
-    }
+vector<int> searchRange(vector<int>& nums, int target) {
+    int left = leftSide(nums, target);
+    int right = rightSide(nums, target);
+    
+    return {left, right};
+}
 
 int main() {
     vector<int> nums = {5,7,7,8,8,10};
