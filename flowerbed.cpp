85920d4e2a18ccd72a5549643306ef9599fac70f
Yash <yashgoel585@gmail.com>
2024-12-26 16:15:01 +0530

    daily

--- flowerbed.cpp
+++ flowerbed.cpp
@@ -3,26 +3,28 @@
 using namespace std;
 
 bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-    for(int i = 0; i < flowerbed.size(); i = i+2) {
-        if(n == 0) 
-            return true;
-
-        if((i == 0 && flowerbed[i] == 0) && flowerbed[i + 1] == 0) {
-            n--;
-        } else if((i == flowerbed.size() - 1 && flowerbed[i] == 0) && flowerbed[i - 1] == 0) {
-            n--;
-        }else {
-            if(flowerbed[i] == 0 && (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)) {
-                n--;
+    int size = flowerbed.size();
+    
+    for (int i = 0; i < size; ++i) {
+        if (n == 0) {
+            return true; 
+        }
+        
+        if (flowerbed[i] == 0) {
+            bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0); 
+            bool rightEmpty = (i == size - 1) || (flowerbed[i + 1] == 0); 
+            
+            if (leftEmpty && rightEmpty) {
+                flowerbed[i] = 1; 
+                --n;
+                i++; 
             }
         }
     }
-
-    if(n == 0) 
-        return true;
-
-    return false;
+    
+    return n == 0; 
 }
+
 
 int main() {
     vector<int> flowerbed = {0,1,0};
